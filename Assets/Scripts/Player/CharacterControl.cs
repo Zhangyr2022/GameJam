84de7353f2cb66a1f6db55d17b0d10fbf3339b9d
Zhangyr2022 <zhangyr21@mails.tsinghua.edu.cn>
2023-05-27 23:48:59 +0800

    feat: change audios

--- Assets/Scripts/Player/CharacterControl.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CharacterControl : MonoBehaviour
-{
-    public Vector3 Movement;
-    public Vector3 LookDirection;
-}
